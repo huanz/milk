3eec49abfe652ac8cf3f1e74f08e85e55547369b
bukas <bukas@bukas.local>
2018-07-24 18:58:16 +0800

    增加跨域

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const serve = require('koa-static');
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 
 // 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
 require('./cloud');
@@ -23,6 +24,8 @@ app.use(router.routes());
 app.use(AV.koa());
 
 app.use(bodyParser());
+// 跨域
+app.use(cors());
 
 app.use(require('./routes/api').routes());
 
